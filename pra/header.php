59354e601cba70394891df2adf9e22def674a086
Kiran <kiranv04@gmail.com>
2024-02-28 22:30:34 +0530

    Initial project setup

---
+++ pra/header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>iBird | All Dubai Items Supplied</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/logo/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500&family=Jost:wght@500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" integrity="sha512-HXXR0l2yMwHDrDyxJbrMD9eLvPe3z3qL3PPeozNTsiHJEENxx8DH2CxmV05iwG0dwoz5n4gQZQyYLUNt1Wdgfg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <style>
+        .phone-icon {
+            font-size: 24px;
+            /* Adjust the font size as needed */
+            color: #007BFF;
+            /* Change the color to your preferred style */
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+            /* Add a subtle shadow */
+        }
+        .navbar {
+            background: linear-gradient(to bottom, #faf339, #299e23) !important;
+            color: white;
+        }
+        .footer {
+            background: linear-gradient(to bottom, #faf339, #299e23) !important;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid bg-white p-0">
+        <!-- Navbar & Hero Start -->
+        <div class="container-fluid bg-white position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0 sticky-top shadow-sm">
+                <a href="" class="navbar-brand p-0">
+                    <img src="img/logo/pragathi_new.png" alt="Logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav mx-auto py-0 pl-50">
+                        <a href="index.php" class="nav-item nav-link active">Home</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Fragrances</a> 
+                            <div class="dropdown-menu m-0">
+                                <a href="" class="dropdown-item">Owdh</a>
+                                <a href="" class="dropdown-item">Perfume</a>
+                                <a href="" class="dropdown-item">Attar</a>
+                            </div>
+                        </div>
+                        <a href="about.php" class="nav-item nav-link">Watches</a>
+                        <a href="about.php" class="nav-item nav-link">Cosmetics</a>
+                        <a href="about.php" class="nav-item nav-link">Jewellery</a>
+                        <a href="about.php" class="nav-item nav-link">Pure Coconut Oil</a>
+                        <a href="about.php" class="nav-item nav-link">About Us</a>
+                    </div>
+                    <a href="contact.php" class="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Get in Touch!</a>
+                </div>
+            </nav>
+        </div>
+        <!-- Navbar & Hero End -->
